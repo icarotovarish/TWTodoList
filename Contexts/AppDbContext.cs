7611a641f01700e6113af74a7cef9a93d5ad0c5d
Ícaro Tavares <icarosoarestavares@gmail.com>
2022-09-26 18:37:06 -0300

    DbContext com metodos

--- Contexts/AppDbContext.cs
+++ Contexts/AppDbContext.cs
@@ -1,8 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using TWTodoList.EntiyConfigs;
 using TWTodoList.Models;
 namespace TWTodoList.Contetxs;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Todo>
+    public DbSet<Todo> Todos => Set<Todo>();
+    protected override void OnConfiguring (DbContextOptionsBuilder builder)
+    {
+        builder.UseSqlServer("Server=Localhost;Database=TWTodoList; Trusted_Connection = true");
+    }
+    protected override void OnModelCreating (ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new TodoEntityConfig());
+    }
 }
